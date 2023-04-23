4bcb07c9356a95a5aceb44055fd63b5e40d606a4
Adrien <asublon@medialta.com>
2023-04-23 12:16:45 +0200

    Updates

---
+++ precache-manifest.5aa8ae5d6432b9f93dba29576bea1671.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac77d6e25440f8642567",
+    "url": "/pipoun/src/index.js/static/css/main.d4344f7b.chunk.css"
+  },
+  {
+    "revision": "ac77d6e25440f8642567",
+    "url": "/pipoun/src/index.js/static/js/main.ac77d6e2.chunk.js"
+  },
+  {
+    "revision": "d7c98ab70d5c9d904d64",
+    "url": "/pipoun/src/index.js/static/js/1.d7c98ab7.chunk.js"
+  },
+  {
+    "revision": "6e7882f98f8de027234c",
+    "url": "/pipoun/src/index.js/static/css/2.3f00d088.chunk.css"
+  },
+  {
+    "revision": "6e7882f98f8de027234c",
+    "url": "/pipoun/src/index.js/static/js/2.6e7882f9.chunk.js"
+  },
+  {
+    "revision": "5f754a40f4cbce36ad46",
+    "url": "/pipoun/src/index.js/static/js/runtime~main.5f754a40.js"
+  },
+  {
+    "revision": "0704d6f624c18fe60dd880ea48fe28c1",
+    "url": "/pipoun/src/index.js/static/media/PipounSite2.0704d6f6.png"
+  },
+  {
+    "revision": "8fc3aff99bf3a8033b43b94af62a1341",
+    "url": "/pipoun/src/index.js/static/media/homade.8fc3aff9.otf"
+  },
+  {
+    "revision": "fb7141cb48c70e979c969ba62e41f63f",
+    "url": "/pipoun/src/index.js/static/media/newLogo.fb7141cb.png"
+  },
+  {
+    "revision": "5f9f9189b7524c41c02fb8270d1a6f54",
+    "url": "/pipoun/src/index.js/static/media/7ven8ightNFT.5f9f9189.png"
+  },
+  {
+    "revision": "6fce00c9f635b9fc1e91173ae40712ce",
+    "url": "/pipoun/src/index.js/static/media/IggyRockNFT.6fce00c9.png"
+  },
+  {
+    "revision": "beb5144f4ee94ea27ca15b40b6a8db5c",
+    "url": "/pipoun/src/index.js/static/media/KnockTysonNFT.beb5144f.png"
+  },
+  {
+    "revision": "9f9e5dd851d87cb128341f6128377469",
+    "url": "/pipoun/src/index.js/static/media/OprahWinfreshNFT.9f9e5dd8.png"
+  },
+  {
+    "revision": "f280b6cfd039518f2ca1473d7dc7a27c",
+    "url": "/pipoun/src/index.js/static/media/SatOrChillNFT.f280b6cf.png"
+  },
+  {
+    "revision": "9c725ca3b30754dc60839530bce16e4b",
+    "url": "/pipoun/src/index.js/static/media/JeffPesosNFT.9c725ca3.png"
+  },
+  {
+    "revision": "5ea4418255b04039a301ee413ebc1e04",
+    "url": "/pipoun/src/index.js/static/media/SnoopDrugNFT.5ea44182.png"
+  },
+  {
+    "revision": "bce4569a8f33babf4f6d154fe6f143f2",
+    "url": "/pipoun/src/index.js/static/media/soucoupe.bce4569a.svg"
+  },
+  {
+    "revision": "4e47a887c948eb7c96a1320af02a32e0",
+    "url": "/pipoun/src/index.js/static/media/PipounSite1.4e47a887.png"
+  },
+  {
+    "revision": "72fba571c4b26ba67bcf8836dc547f9c",
+    "url": "/pipoun/src/index.js/static/media/pre.72fba571.svg"
+  },
+  {
+    "revision": "889ebbd119a712bf1d826bf56e73e7ff",
+    "url": "/pipoun/src/index.js/index.html"
+  }
+];
