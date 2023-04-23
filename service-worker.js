c0a063a09c6ef0cd9225216954c71cdef1773877
Adrien <asublon@medialta.com>
2023-04-23 12:22:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pipoun/index.js/precache-manifest.061734da105d5fb7ae868738b8dd6f45.js"
+  "/pipoun/precache-manifest.98e2e28379632b0b07b2229f6657b992.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pipoun/index.js/index.html", {
+workbox.routing.registerNavigationRoute("/pipoun/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
